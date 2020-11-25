47eed9afc89cb7b468fd6b51cae888d3b2dd0600
Alfredo Deza <adeza@anchore.com>
2020-11-25 14:47:34 -0500

    tests: update localanchore to not use removed exception
    Signed-off-by: Alfredo Deza <adeza@anchore.com>

--- tests/unit/anchore_engine/clients/test_localanchore_standalone.py
+++ tests/unit/anchore_engine/clients/test_localanchore_standalone.py
@@ -1,6 +1,5 @@
 import pytest
-import os
-from anchore_engine.clients.localanchore_standalone import retrying_pull_image, ImagePullError
+from anchore_engine.clients.localanchore_standalone import retrying_pull_image, AnalysisError
 from anchore_engine.clients import localanchore_standalone
 from anchore_engine.subsys import logger
 
@@ -13,7 +12,7 @@ fail_threshold = 2
 def always_fail(*args, **kwargs):
     global fail_counter
     fail_counter += 1
-    raise ImagePullError(pull_string='testing', tag='latest', cause=Exception('cannot pull'))
+    raise AnalysisError(msg='', pull_string='testing', tag='latest', cause=Exception('cannot pull'))
 
 
 def fail_twice(*args, **kwargs):
@@ -21,7 +20,7 @@ def fail_twice(*args, **kwargs):
 
     if fail_counter < fail_threshold:
         fail_counter += 1
-        raise ImagePullError(pull_string='somepullstring', tag='latest', cause=Exception('cannot pull'))
+        raise AnalysisError(msg='', pull_string='somepullstring', tag='latest', cause=Exception('cannot pull'))
     else:
         return True
 
@@ -44,7 +43,7 @@ def test_retrying_image_pull_full_failure(alwaysfail_pull):
     """
 
     global fail_counter
-    with pytest.raises(ImagePullError):
+    with pytest.raises(AnalysisError):
         retrying_pull_image(staging_dirs={}, pullstring='somepullstring', registry_creds=[], manifest=None, parent_manifest=None)
 
     assert fail_counter == 3
